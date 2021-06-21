086d5c6a5201882c8508a980b3e6531c37ab1a8e
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-06-21 10:12:44 -0500

    updatiing files

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,7 +16,7 @@ function renderLicenseBadge(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
-  ${generateLicense(data.projectLicense)}
+  ${renderLicenseBadge(data.projectLicense)}
   ## Description
   ${data.projectDescription}
   
